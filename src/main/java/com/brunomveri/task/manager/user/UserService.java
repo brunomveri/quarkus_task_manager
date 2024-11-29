93a31e9785fd99d96797a16173129cb339d45c3d
Bruno Verissimo <bruno.verissimo@justeattakeaway.com>
2024-11-29 10:16:31 -0700

    added create, update, delete user and get current user methods

--- src/main/java/com/brunomveri/task/manager/user/UserService.java
+++ src/main/java/com/brunomveri/task/manager/user/UserService.java
@@ -1,7 +1,11 @@
 package com.brunomveri.task.manager.user;
 
+import com.brunomveri.task.manager.project.Project;
+import com.brunomveri.task.manager.task.Task;
+import io.quarkus.elytron.security.common.BcryptUtil;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.transaction.Transactional;
 import org.hibernate.ObjectNotFoundException;
 
 import java.util.List;
@@ -24,5 +28,32 @@ public class UserService {
         return User.listAll();
     }
 
+    @Transactional
+    public Uni<User> create(User user) {
+        user.password = BcryptUtil.bcryptHash(user.password);
+        return user.persistAndFlush();
+    }
 
+    @Transactional
+    public Uni<User> update(User user) {
+        return findById(user.id)
+                .chain(u -> User.getSession())
+//                .chain(User::getSession)
+                .chain(s -> s.merge(user));
+    }
+
+    @Transactional
+    public Uni<Void> delete(long id) {
+        return findById(id)
+                .chain(u -> Uni.combine().all().unis(
+                        Task.delete("user.id", u.id),
+                        Project.delete("user.id", u.id))
+                               .asTuple()
+                               .chain(t -> u.delete()));
+    }
+
+    public Uni<User> getCurrentUser() {
+        // TODO: replace implementation once security is added to the project
+        return User.find("order by ID").firstResult();
+    }
 }
