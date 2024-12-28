0a9f7c6ae51883c3385e4b9fa882004a2ec7f260
Bruno Verissimo <bruno.verissimo@justeattakeaway.com>
2024-12-27 19:24:38 -0700

    create user resource class

---
+++ src/main/java/com/brunomveri/task/manager/user/UserResource.java
@@ -0,0 +1,51 @@
+package com.brunomveri.task.manager.user;
+
+import io.smallrye.mutiny.Uni;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.core.MediaType;
+import org.jboss.resteasy.reactive.ResponseStatus;
+
+import java.util.List;
+
+@Path("/api/v1/users")
+public class UserResource {
+
+    private final UserService userService;
+
+    @Inject
+    public UserResource(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GET
+    public Uni<List<User>> get() {
+        return userService.list();
+    }
+
+    @GET
+    @Path("{id}")
+    public Uni<User> get(@PathParam("id") long id) {
+        return userService.findById(id);
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @ResponseStatus(201)
+    public Uni<User> create(User user) {
+        return userService.create(user);
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("{id}")
+    public Uni<User> update(@PathParam("id") long id, User user) {
+        user.id = id;
+        return userService.update(user);
+    }
+}
